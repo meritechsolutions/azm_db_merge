10397869d033a304a13278c302bcedaa4634fefa
Kasidit Yusuf <ykasidit@gmail.com>
2016-12-13 10:43:08 +0700

    add mssql example bat files

---
+++ ex_mssql_import_azm.bat
@@ -0,0 +1,2 @@
+python azm_db_merge.py --azm_file "example_logs/358096071732800 12_12_2016 13.44.10.azm" --target_db_type mssql --server_user azqdblogin --server_password pass --server_database azqdb
+pause
---
+++ ex_mssql_import_azm_folder.bat
@@ -0,0 +1,2 @@
+python azm_db_merge.py --azm_file "example_logs/" --target_db_type mssql --server_user azqdblogin --server_password pass --server_database azqdb
+pause

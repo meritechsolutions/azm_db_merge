60796be48e3a1ccb14ee85372649b245340240b0
Kasidit Yusuf <ykasidit@gmail.com>
2017-01-06 15:07:35 +0700

    new log dbs use 'log_hash' instead of imei_id + start/end time, new geom blobs still not handled - planning for wkb into postgis

--- ex_mssql_remove_azm.bat
+++ ex_mssql_remove_azm.bat
@@ -1,2 +1,2 @@
-python azm_db_merge.py --unmerge --azm_file "example_logs/358096071732800 12_12_2016 13.44.10.azm" --target_db_type mssql --server_user azqdblogin --server_password pass --server_database azqdb
+python azm_db_merge.py --unmerge --azm_file "example_logs/358096071732800 2_1_2017 13.12.49.azm" --target_db_type mssql --server_user azqdblogin --server_password pass --server_database azqdb
 pause
--- ex_postgre_merge_azm.sh
+++ ex_postgre_merge_azm.sh
@@ -1,1 +1,1 @@
-python azm_db_merge.py --azm_file "example_logs/358096071732800 12_12_2016 13.44.10.azm" --target_db_type postgresql --server_user kasidit --server_database azqdb --server_password pass
+python azm_db_merge.py --azm_file "example_logs/358096071732800 2_1_2017 13.12.49.azm" --target_db_type postgresql --server_user kasidit --server_database azqdb --server_password pass

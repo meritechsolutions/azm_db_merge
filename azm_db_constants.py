2a223f6d59096cd48f7e9d94f80a65a24d473c84
Kasidit Yusuf <ykasidit@gmail.com>
2023-04-21 06:11:07 +0700

    update mssql support cases

--- azm_db_constants.py
+++ azm_db_constants.py
@@ -7,8 +7,8 @@ Copyright: Copyright (C) 2016 Freewill FX Co., Ltd. All rights reserved.
 
 #the PARAMs for other programs like sql server bulk insert format file so the '\n' must be '\\n' so it finally shows up as '\n' etc
   
-BULK_INSERT_LINE_SEPARATOR_VALUE = "|\n"
-BULK_INSERT_LINE_SEPARATOR_PARAM = "|\\n" # like c_bitwise_or, also called pipe character
+BULK_INSERT_LINE_SEPARATOR_VALUE = "!\n"
+BULK_INSERT_LINE_SEPARATOR_PARAM = "!\\n" # like c_bitwise_or, also called pipe character
 
-BULK_INSERT_COL_SEPARATOR_VALUE = "\t"
-BULK_INSERT_COL_SEPARATOR_PARAM = "\\t"
+BULK_INSERT_COL_SEPARATOR_VALUE = "\t!"
+BULK_INSERT_COL_SEPARATOR_PARAM = "\\t!"
